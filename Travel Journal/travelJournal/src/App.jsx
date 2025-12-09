91328ddb5c0751b9ddc1d5b4d958a276e570f65c
samraddhi sharma <samraddhisharma1216@gmail.com>
2025-12-09 19:26:41 +0530

    Add Travel Journal project

---
+++ Travel Journal/travelJournal/src/App.jsx
@@ -0,0 +1,52 @@
+
+import './App.css'
+import HeaderSection from './components/header'
+import Entry from './components/entry'
+import './components/entry.css'
+import data from './components/data'
+
+function App(){
+  const entryElements= data.map((entry)=>{
+    return (
+      <Entry
+      key={entry.id}
+      {...entry}
+      />
+
+    )
+  })
+  return(
+  <>
+       <HeaderSection/>
+       <main className='container'>
+       {entryElements}
+       </main>
+        {/* <Entry
+         imgsrc="https://media.istockphoto.com/id/1155675399/photo/pangong-lake.webp?a=1&b=1&s=612x612&w=0&k=20&c=iuWa4BWFoycpi-__qg7XIO2bOS5hSv2dzD17L9V555U=" 
+         Gmaplink="https://maps.app.goo.gl/zgbYh72oL7kVZpVk7"
+         countryName="India"
+         placename="Pangong Lake"
+         placeinfo="Pangong Tso(lake) is situated at a height of 14,270ft above sea level and is said to be one of the highest brackish water lakes in the world. The lake is almost 5km wide and 134km long, with one-third of it lying in India and the other two-thirds in China. 
+         The beauty is adorned with furry milky clouds and the overlooking stern mountains making an artistic impression reflecting in the water. The lake is also a breeding ground for different types of birds during the summer season. Bar-headed geese, Brahminy ducks, black necked cranes and seagulls are commonly spotted here. The most gripping fact about this lake is that it changes color appearing blue, grey-green, and red at varied points in time. During winter, the lake freezes completely despite being saline water."
+       />
+        <Entry
+          imgsrc="https://media.istockphoto.com/id/1315260875/photo/aerial-view-over-a-tropical-forest-with-a-river-meandering-through-the-canopy-and-a-clouded.jpg?s=612x612&w=0&k=20&c=D-JOMC6tQVIJxNqmWwv8nOxagqqwBtsZK7dhAQePwgw="
+          Gmaplink= "https://maps.app.goo.gl/kMVzwRgXP5QCG8tT6"
+          countryName="South America"
+          placename="Amazon Rainforest"
+          placeinfo="Covering roughly 40 percent of South America, including parts of Brazil, Peru, and Colombia, the Amazon is the largest rainforest on the planet, and home to more than 40,000 plant species and 1,300 bird species alone. But be sure to visit the winding rivers and 
+          diverse wildlife while you can—climate change (along with man-made fires) is increasingly whittling away the habitat every day."
+        />
+      <Entry
+         imgsrc="https://media.istockphoto.com/id/584209898/photo/beautiful-positano-amalfi-coast-italy.jpg?s=612x612&w=0&k=20&c=jNO6AeW-uP98lcaZ_HlMmTPwWl64-hrBTVPhfRPjh9s="
+         Gmaplink="https://maps.app.goo.gl/Wp6YrYEAEi7rdKNX6"
+         countryName="Italy"
+         placename="Amalfi Coast"
+         placeinfo="The Amalfi Coast, with its dramatic cliffs, colorful villages, and azure sea, is one of Italy's most stunning coastal regions. Drive along the scenic Amalfi Drive, visit the charming towns of Positano and Ravello, and explore the historic sites of Amalfi. 
+         The region's lemon groves, terraced vineyards, and Mediterranean cuisine add to its allure. 
+         The Amalfi Coast's breathtaking views and romantic ambiance make it a top destination for travellers."
+      /> */}
+    </>
+  )
+}  
+export default App
